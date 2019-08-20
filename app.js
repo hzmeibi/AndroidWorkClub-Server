900aff3b29cf19949038cba6b49c8f9018d3d247
Milo <milomei@163.com>
2019-08-20 10:32:36 +0800

    不知道改了啥

--- app.js
+++ app.js
@@ -47,7 +47,11 @@ app.all('*', function (req, res, next) {
   res.set('Access-Control-Allow-Credentials', true);
   res.set('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
   res.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization');//'X-Requested-With, Content-Type, Authorization');
-
+ 
+ //res.header("Access-Control-Allow-Origin", "*");
+  //res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  //res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  //res.header("X-Powered-By",' 3.2.1');
   res.header("Content-Type", "application/json;charset=utf-8");
   next();
 });
